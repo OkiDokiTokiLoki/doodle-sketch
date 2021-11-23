5310b16fad4a86bf7fba74fe667e56e3e355515d
tokiloki <codebadger1@gmail.com>
2021-11-23 11:32:59 +0200

    button styling added

--- script.js
+++ script.js
@@ -7,28 +7,14 @@ function buildGrid(rows, cols){
 
     for(let i = 0; i < (rows * cols); i++){
         let gridCell = document.createElement('div');
-        gridCell.addEventListener('mouseover', drawC);
-        // gridCell.addEventListener('toggle', drawT);
+        gridCell.addEventListener('mouseover', draw);
         container.appendChild(gridCell).className = 'grid-item';
     };
 };
 
-function drawC(e){
-    // let key = e.keyCode;
-
-    e.target.style.background = 'red';
-
-    // while (key == 16){
-    //     e.target.style.background = 'red';
-    // }
-    
+function draw(e){
+    e.target.style.background = 'red';    
 };
-
-// function drawT(e){
-//     e.target.classList.toggle('penOn');
-//     e.target.classList.toggle('penOff');
-//     console.log(e.target);
-// };
 
 // buildGrid(8, 8);
  buildGrid(16, 16);
