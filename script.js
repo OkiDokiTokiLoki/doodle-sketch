7afd6fd66cfc6532b341d3bf84d79d016fdfd0a7
tokiloki <codebadger1@gmail.com>
2021-11-23 11:26:45 +0200

    basic restart implemented

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('#grid');
+const restartBtn = document.querySelector('#restart');
 
 function buildGrid(rows, cols){
     container.style.setProperty('--grid-rows', rows);
@@ -6,15 +7,35 @@ function buildGrid(rows, cols){
 
     for(let i = 0; i < (rows * cols); i++){
         let gridCell = document.createElement('div');
-        gridCell.addEventListener('mouseover', draw);
+        gridCell.addEventListener('mouseover', drawC);
+        // gridCell.addEventListener('toggle', drawT);
         container.appendChild(gridCell).className = 'grid-item';
     };
 };
 
-function draw(e){
+function drawC(e){
+    // let key = e.keyCode;
+
     e.target.style.background = 'red';
+
+    // while (key == 16){
+    //     e.target.style.background = 'red';
+    // }
+    
 };
+
+// function drawT(e){
+//     e.target.classList.toggle('penOn');
+//     e.target.classList.toggle('penOff');
+//     console.log(e.target);
+// };
 
 // buildGrid(8, 8);
  buildGrid(16, 16);
 // buildGrid(64, 64);
+
+restartBtn.addEventListener('click', restart)
+
+function restart(){
+    window.location.reload();
+}
