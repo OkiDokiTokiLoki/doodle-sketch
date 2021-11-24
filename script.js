f4340e57cc6b931c78526565a2eb5ac031ebea8d
tokiloki <codebadger1@gmail.com>
2021-11-24 10:45:21 +0200

    draw functionality complete

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 const container = document.querySelector('#grid');
 const restartBtn = document.querySelector('#restart');
 
+let penColor = 'red';
+
 function buildGrid(rows, cols){
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
 
     for(let i = 0; i < (rows * cols); i++){
         let gridCell = document.createElement('div');
-        gridCell.addEventListener('mouseover', draw);
+        gridCell.addEventListener('mousedown', drawClick);
+        gridCell.addEventListener('mouseenter', drawDrag);
         container.appendChild(gridCell).className = 'grid-item';
     };
 };
 
-function draw(e){
-    e.target.style.background = 'red';    
+function drawClick(e){
+    e.target.style.background = penColor;    
+};
+
+function drawDrag(e){
+    if (e.buttons > 0){
+        e.target.style.background = penColor;  
+    }  
 };
 
 // buildGrid(8, 8);
