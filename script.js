c7ddc34ef96852cbdf514234f7240229c72f3a1e
tokiloki <codebadger1@gmail.com>
2021-11-26 11:31:17 +0200

    penColors implemented

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const coolPen = document.querySelector('#cool');
 // const gridMedium = document.querySelector('#gridMedium');
 // const gridLarge = document.querySelector('#gridLarge');
 
+const warmPalette = ['#fafa6e', '#e6b54a', '#c17638', '#903f2b', '#570f1c'];
+const coolPalette = ['#56a25a', '#009e87', '#0093c2', '#007fe9', '#5057db'];
+
 let penColor = '#000';
 let erasePen = false;
 let warmBool = false;
@@ -55,6 +58,25 @@ pen.addEventListener('input', (e) => {
 });
 
 
+// const colors = [ 'blue', 'yellow', 'orange', 'red' ];
+// const randIndex = Math.floor(Math.random() * colors.length);
+
+// console.log(colors[randIndex]);
+
+// const randomWarm = () => {
+//     return Math.floor(Math.random() * warmPalette.length);
+// };
+
+const randomWarm = Math.floor(Math.random() * warmPalette.length);
+
+// const randomCool = () => {
+//     return Math.floor(Math.random() * coolPalette.length);
+// };
+
+// console.log(randomCool());
+// const randomCool = Math.floor(Math.random() * coolPalette.length);
+
+
 const randColor = () =>  {
     return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
@@ -89,9 +111,9 @@ function drawClick(e){
     if (erasePen){
         e.target.style.backgroundColor = '';
     } else if (warmBool){
-        e.target.style.backgroundColor = randColor();
+        e.target.style.backgroundColor = warmPalette[randomWarm];
     } else if (coolBool){
-        e.target.style.backgroundColor = 'blue';
+        e.target.style.backgroundColor = randColor();
     }
     else{
         e.target.style.backgroundColor = penColor;
@@ -104,9 +126,9 @@ function drawDrag(e){
         if (erasePen){
             e.target.style.backgroundColor = '';
         } else if (warmBool){
-            e.target.style.backgroundColor = randColor();
+            e.target.style.backgroundColor = warmPalette[randomWarm];
         } else if (coolBool){
-            e.target.style.backgroundColor = 'blue';
+            e.target.style.backgroundColor = randColor();
         }
         else{
             e.target.style.backgroundColor = penColor;
