d28686f52d53d1b72e1cfe46e87b8d4c9a850ff2
tokiloki <codebadger1@gmail.com>
2021-11-28 13:29:16 +0200

    warmPen functionality implemented

--- script.js
+++ script.js
@@ -5,20 +5,17 @@ const eraser = document.querySelector('#eraser');
 const warmPen = document.querySelector('#warm');
 const coolPen = document.querySelector('#cool');
 const rainbowPen = document.querySelector('#rainbow');
+
+const buttons = document.querySelectorAll('button');
+
 //const gridSizeDisplay = document.querySelector('#gridSizeDisplay');
 //const gridSize = document.querySelector('#gridSize');
-// const gridSmall = document.querySelector('#gridSmall');
-// const gridMedium = document.querySelector('#gridMedium');
-// const gridLarge = document.querySelector('#gridLarge');
+const gridSmall = document.querySelector('#gridSmall');
+const gridMedium = document.querySelector('#gridMedium');
+const gridLarge = document.querySelector('#gridLarge');
 
 const warmPalette = ['#fafa6e', '#e6b54a', '#c17638', '#903f2b', '#570f1c'];
 const coolPalette = ['#56a25a', '#009e87', '#0093c2', '#007fe9', '#5057db'];
-
-let penColor = '#000';
-let erasePen = false;
-let warmBool = false;
-let coolBool = false;
-let rainbowBool = false;
 
 
 function buildGrid(rows, cols){
@@ -33,57 +30,53 @@ function buildGrid(rows, cols){
     };
 };
 
-buildGrid(8, 8); // default grid
+buildGrid(16, 16); // default grid
 
-// gridSmall.addEventListener('click', () => {
-//     buildGrid(8,8);
+gridSmall.addEventListener('click', () => {
+    buildGrid(8,8);
 
-// });
+});
 
-// gridMedium.addEventListener('click', () => {
-//     buildGrid(16,16);
+gridMedium.addEventListener('click', () => {
+    buildGrid(16,16);
 
-// });
+});
 
-// gridLarge.addEventListener('click', () => {
-//     buildGrid(32,32);
+gridLarge.addEventListener('click', () => {
+    buildGrid(32,32);
 
-// });
+});
 
 // buildGrid(8, 8);
 // buildGrid(16, 16);
 // buildGrid(64, 64);
 
 // Set penColor
+let penColor = '#000';
+
 pen.addEventListener('input', (e) => {
     penColor = e.target.value;
 });
 
-// const randomWarm = () => {
-//     return Math.floor(Math.random() * warmPalette.length);
-// };
+// warmPen functionality
+let warmBool = false;
 
-// const randomWarm = Math.floor(Math.random() * warmPalette.length);
+warmPen.addEventListener('click', () => {
+    if (warmBool){
+        warmPen.style.backgroundColor = '';
+        warmBool = false;
+    } else{
+        warmPen.style.backgroundColor = 'var(--medium)';
+        warmBool = true;
+    }
+});
 
-// const randomCool = () => {
-//     return Math.floor(Math.random() * coolPalette.length);
-// };
+const randomWarm = () => {
+    return warmPalette[Math.floor(Math.random() * warmPalette.length)];
+};
 
-// console.log(randomCool());
-// const randomCool = Math.floor(Math.random() * coolPalette.length);
-
-
-const randColor = () =>  {
-    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
-}
-
-// warmPen.addEventListener('click', () => {
-//     if (warmBool){
-//         warmBool = false;
-//     } else{
-//         warmBool = true;
-//     }
-// });
+// coolPen functionality
+let coolBool = false;
 
 // coolPen.addEventListener('click', () => {
 //     if (coolBool){
@@ -93,19 +86,32 @@ const randColor = () =>  {
 //     }
 // });
 
+// RainbowPen functionality
+let rainbowBool = false;
+
 rainbowPen.addEventListener('click', () => {
     if (rainbowBool){
+        rainbowPen.style.backgroundColor = '';
         rainbowBool = false;
     } else{
+        rainbowPen.style.backgroundColor = 'var(--medium)';
         rainbowBool = true;
     }
 });
 
+const rainbowColor = () =>  {
+    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
+}
+
 // Erase functionality
+let erasePen = false;
+
 eraser.addEventListener('click', () => {
     if (erasePen){
+        eraser.style.backgroundColor = '';
         erasePen = false;
     } else{
+        eraser.style.backgroundColor = 'var(--medium)';
         erasePen = true;
     }
 });
@@ -114,12 +120,12 @@ eraser.addEventListener('click', () => {
 function drawClick(e){
     if (erasePen){
         e.target.style.backgroundColor = '';
-    // } else if (warmBool){
-    //     e.target.style.backgroundColor = warmPalette[randomWarm];
+    } else if (warmBool){
+        e.target.style.backgroundColor = randomWarm();
     // } else if (coolBool){
-    //     e.target.style.backgroundColor = randColor();
+    //     e.target.style.backgroundColor = rainbowColor();
     } else if (rainbowBool){
-        e.target.style.backgroundColor = randColor();
+        e.target.style.backgroundColor = rainbowColor();
     }
     else{
         e.target.style.backgroundColor = penColor;
@@ -131,12 +137,12 @@ function drawDrag(e){
     if (e.buttons > 0){
         if (erasePen){
             e.target.style.backgroundColor = '';
-        // } else if (warmBool){
-        //     e.target.style.backgroundColor = warmPalette[randomWarm];
+        } else if (warmBool){
+            e.target.style.backgroundColor = randomWarm();
         // } else if (coolBool){
-        //     e.target.style.backgroundColor = randColor();
+        //     e.target.style.backgroundColor = rainbowColor();
         } else if (rainbowBool){
-            e.target.style.backgroundColor = randColor();
+            e.target.style.backgroundColor = rainbowColor();
         }
         else{
             e.target.style.backgroundColor = penColor;
